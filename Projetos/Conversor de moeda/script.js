39b865430ef2827c93d793e2ba86af37848fcf70
João Vitor <joaovitonrnascim@gmail.com>
2023-06-12 21:41:08 -0300

    I end my new project

--- Projetos/Conversor de moeda/script.js
+++ Projetos/Conversor de moeda/script.js
@@ -1,14 +1,68 @@
 const convertButton = document.querySelector(".submit-button")
 const currencyValue = document.querySelector(".converted-value")
-
+const valueChoosed1 = document.querySelector("#result-value1")
+const valueChoosed2 = document.querySelector("#result-value2")
+const chosenCurrencyToConvert = document.querySelector("#convertValues")
+const convertedImg = document.querySelector("#converted-img")
+const convertedParagraph = document.querySelector("#converterd-paragraph")
 
 function convertValue() {
   const valueToConvert = currencyValue.value
   const dolarToday = 4.86
+  const euroToday = 5.23
+  const bitCoinToday = 125.889
+  let valueConverted
 
-  const valueConverted = valueToConvert / dolarToday
+  if (chosenCurrencyToConvert.value === "Dolar") {
+    valueConverted = valueToConvert / dolarToday
 
-  console.log(valueConverted.toFixed(2))
+    valueChoosed1.innerHTML = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(valueToConvert)
+
+    valueChoosed2.innerHTML = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(valueConverted)
+  } else if (chosenCurrencyToConvert.value === "Euro") {
+    valueConverted = valueToConvert / euroToday
+
+    valueChoosed1.innerHTML = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(valueToConvert)
+
+    valueChoosed2.innerHTML = new Intl.NumberFormat('en-UK', {
+      style: 'currency',
+      currency: 'EUR'
+    }).format(valueConverted)
+  } else if (chosenCurrencyToConvert.value === "Bitcoin") {
+    valueConverted = valueToConvert / bitCoinToday
+
+    valueChoosed1.innerHTML = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(valueToConvert)
+
+    valueChoosed2.innerHTML = new Intl.NumberFormat('de-DE', {
+      style: 'currency',
+      currency: 'BTC'
+    }).format(valueConverted)
+  }
 }
 
-convertButton.addEventListener("click", convertValue)
+function changeCurrency() {
+  if (chosenCurrencyToConvert.value === "Euro") {
+    convertedImg.src = "./assets/euro.svg"
+    convertedParagraph.innerHTML = "Euro"
+    valueChoosed2.innerHTML = "€ 0.00"
+  } if (chosenCurrencyToConvert.value === "Bitcoin") {
+    convertedImg.src = "./assets/bitcoin.png"
+    convertedParagraph.innerHTML = "Bitcoin"
+    valueChoosed2.innerHTML = "0.00 BTC"
+  }
+}
+
+chosenCurrencyToConvert.addEventListener("change", changeCurrency)
+convertButton.addEventListener('click', convertValue)
