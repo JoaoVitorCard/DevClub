302a0b8efd197716ef7cccdf40679c29b9048295
João Vitor <joaovitonrnascim@gmail.com>
2023-06-15 21:37:37 -0300

    Start a new project with random numbers

---
+++ JavaScript-pt2/Math.js
@@ -0,0 +1,11 @@
+/* 
+  Math -> Matemática
+
+  - pow(2,2) / power / potência / 2² = 4 
+  - sqrt(25,2) / square / raiz / √25
+  - PI
+  - ceil / teto -> arredonda para cima
+  - floor / chão -> arredonda para baixo
+
+  - random() / número aleatório entre 0 e 1
+*/
